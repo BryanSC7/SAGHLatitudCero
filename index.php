52265862f56b31778f75d261a443edae7f530690
user <user@user-PC>
2015-12-16 08:29:58 -0500

    hola

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo 'Hola Index';
-        ?>
-    </body>
-</html>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo 'hola mundo';
+
